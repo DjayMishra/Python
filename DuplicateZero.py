cd90b944948dfb90e5e32953045c8ab1311fa141
Dhananjay Mishra <dhananjay.mishra.in@gmail.com>
2022-10-27 16:40:37 -0400

    Duplicate Zeros in array

---
+++ DuplicateZero.py
@@ -0,0 +1,22 @@
+'''
+Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+Example 1:
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+Example 2:
+Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+'''
+from typing import List
+class DuplicateZero:
+   def Duplicate_Zero(self, arr: List)-> List[int]:
+      for i in reversed(range(0, len(arr)-1)):
+             if arr[i]==0:
+                 for j in reversed(range(i, len(arr)-1)):
+                     arr[j+1]=arr[j]
+
+
+      return arr
--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-'''
-Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
-Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
-Example 1:
-Input: arr = [1,0,2,3,0,4,5,0]
-Output: [1,0,0,2,3,0,0,4]
-Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
-Example 2:
-Input: arr = [1,2,3]
-Output: [1,2,3]
-Explanation: After calling your function, the input array is modified to: [1,2,3]
-'''
+from DuplicateZero import DuplicateZero
 
-nums = [1,0,2,3,0,4,5,0]
-for i in reversed(range(0, len(nums)-1)):
-       if nums[i]==0:
-           for j in reversed(range(i, len(nums)-1)):
-               nums[j+1]=nums[j]
-
-print(nums)
-
+duplicateZero=DuplicateZero()
+array=[1,0,2,3,0,4,5,0]
+print(duplicateZero.Duplicate_Zero(array))
